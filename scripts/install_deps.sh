105f1d61c8c102563cdc14292d7923d8f9d42c7a
mapsam <matthews.sam@gmail.com>
2017-11-27 10:55:07 -0800

    use spatial algorithms dev release

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -16,7 +16,7 @@ install geometry 96d3505
 install variant 1.1.4
 install vtzero 1c38ce7
 install protozero 1.6.0
-install spatial-algorithms 2904283
+install spatial-algorithms cdda174
 install boost 1.65.1
 install cheap-ruler 2.5.3
 install vector-tile 0390175
