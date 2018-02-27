13015afdb43a25e5b0492345605a5d7a80bcf363
mapsam <matthews.sam@gmail.com>
2018-02-27 15:22:04 -0800

    update vtzero

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-v0.18.0}"
+export MASON_RELEASE="${MASON_RELEASE:-35d812c}"
 export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-5.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
